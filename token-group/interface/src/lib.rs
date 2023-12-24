17a638f761529f87382ee1f2fc318cfac45f7e38
Miraland Labs <miraland.labs@gmail.com>
2023-12-24 17:21:56 +0800

    token-group-interface ver bump to 0.1.1

--- token-group/interface/src/lib.rs
+++ token-group/interface/src/lib.rs
@@ -7,5 +7,5 @@ pub mod error;
 pub mod instruction;
 pub mod state;
 
-/// Namespace for all programs implementing spl-token-group
+/// Namespace for all programs implementing solarti-token-group
 pub const NAMESPACE: &str = "spl_token_group_interface";
